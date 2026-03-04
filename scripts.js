792c7b17dbd442228bae1b35ec355be1cb1e7ff8
Adam <143813907+Array22@users.noreply.github.com>
2026-03-04 11:21:45 +1000

    embedded chart widget and added growth value color function

--- scripts.js
+++ scripts.js
@@ -4,5 +4,7 @@ const stockGrowth = document.querySelector("#stock-growth");
 const growthValue = parseFloat(stockGrowth.textContent);
 
 if (growthValue > 0) {
-    
+    stockGrowth.classList.add("text-green-600")
+} else if (growthValue < 0) {
+    stockGrowth.classList.add("text-red-600")
 }
