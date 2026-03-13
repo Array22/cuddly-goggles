dae10c35376a4536a6051371598237311e55490e
Adam <143813907+Array22@users.noreply.github.com>
2026-03-13 23:58:42 +1000

    Add: start on connecting to supabase client

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import os
 from typing import Union
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse, FileResponse
 from pydantic import BaseModel
 from graphing import plot_candlestick
+from supabase import create_client, Client
 # import plotly.io as pio
 
 app = FastAPI()
+
+supabase: Client = create_client(os.environ.get("SUPABASE_URL"),
+                                 os.environ.get("SUPABASE_PUBLISHABLE_KEY"))
 
 class Item(BaseModel):
     """Example item"""
@@ -29,6 +34,10 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/")
 def read_root():
     return FileResponse('test.html')
+
+# @app.get("/table")
+# def read_table():
+#     data = supabase.table()
 
 # @app.get("/charts", response_class=HTMLResponse)
 # def read_charts():
